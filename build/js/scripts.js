1c8cacb8213926bd9dfe1d4de53425b791b08746
Yakup <yakupjoraev@gmail.com>
2024-06-12 09:46:43 +0300

    added new modal

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -120,6 +120,33 @@ function objectGallerySlider() {
 objectGallerySlider();
 
 
+
+const openModalBtns = document.querySelectorAll('.open-modal-btn');
+const closeModalBtns = document.querySelectorAll('.close-modal-btn');
+const modals = document.querySelectorAll('.modal');
+
+openModalBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    const modalId = btn.dataset.modalId;
+    const modal = document.getElementById(modalId);
+    modal.classList.add('show');
+  });
+});
+
+closeModalBtns.forEach(btn => {
+  btn.addEventListener('click', () => {
+    const modal = btn.closest('.modal');
+    modal.classList.remove('show');
+  });
+});
+
+window.addEventListener('click', (event) => {
+  if (event.target.classList.contains('modal')) {
+    event.target.classList.remove('show');
+  }
+});
+
+
 customSelect('select');
 
 new AirDatepicker('#datepicker');
