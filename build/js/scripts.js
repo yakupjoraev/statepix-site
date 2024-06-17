8d1983cd97461b16fc7867ea436d8691ae4ecce0
Yakup <yakupjoraev@gmail.com>
2024-06-17 12:42:36 +0300

    added new fixes and new conponents, pages

--- build/js/scripts.js
+++ build/js/scripts.js
@@ -16,15 +16,19 @@ function burgerMenu() {
       body.classList.remove('locked')
     }
   })
-  //снять классы при клике на элементы меню
-  const menuItems = document.querySelectorAll('.menu__item')
+
+  // Снять классы при клике на элементы меню, кроме элементов с классом dropdown
+  const menuItems = document.querySelectorAll('.menu__item');
 
   menuItems.forEach(item => {
-    item.addEventListener('click', function () {
-      menu.classList.remove('active')
-      burger.classList.remove('active-burger')
-      body.classList.remove('locked')
-    })
+    item.addEventListener('click', function (event) {
+      // Проверяем, имеет ли кликнутый элемент или его родитель класс dropdown
+      if (!item.classList.contains('dropdown')) {
+        menu.classList.remove('active');
+        burger.classList.remove('active-burger');
+        body.classList.remove('locked');
+      }
+    });
   });
 
   // Вот тут мы ставим брейкпоинт навбара
@@ -77,6 +81,61 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 });
+
+document.addEventListener("DOMContentLoaded", function () {
+  const container = document.querySelector('.dropdown');
+
+  if (!container) {
+    return null
+  }
+
+  const dropdowns = document.querySelectorAll(".menu__item.dropdown");
+
+  dropdowns.forEach(dropdown => {
+    const link = dropdown.querySelector(".menu__item-link");
+
+    // Обработчик для клика на ссылку
+    link.addEventListener("click", function (event) {
+      event.preventDefault();
+
+      // Проверяем, есть ли у элемента класс active
+      if (dropdown.classList.contains("active")) {
+        // Если есть, удаляем его
+        dropdown.classList.remove("active");
+      } else {
+        // Удаляем класс active у всех dropdown элементов
+        dropdowns.forEach(item => item.classList.remove("active"));
+        // Добавляем класс active к текущему элементу
+        dropdown.classList.add("active");
+      }
+    });
+
+    // Обработчик для кликов на ссылки внутри выпадающего меню
+    const sublinks = dropdown.querySelectorAll(".menu__sublink");
+    sublinks.forEach(sublink => {
+      sublink.addEventListener("click", function () {
+        dropdown.classList.remove("active");
+      });
+    });
+  });
+
+  // Обработчик для клика вне выпадающего меню
+  document.addEventListener("click", function (event) {
+    let isClickInside = false;
+    dropdowns.forEach(dropdown => {
+      if (dropdown.contains(event.target)) {
+        isClickInside = true;
+      }
+    });
+
+    if (!isClickInside) {
+      dropdowns.forEach(dropdown => {
+        dropdown.classList.remove("active");
+      });
+    }
+  });
+});
+
 
 
 function objectGallerySlider() {
